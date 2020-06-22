a4a0d9f1a990ab3dc0015ceb23fd2c925dd44675
muamar <muamarhumaidi@gmail.com>
2020-06-22 22:38:45 +0700

    view create product

--- resources/views/pages/product/add.blade.php
+++ resources/views/pages/product/add.blade.php
@@ -8,76 +8,85 @@ Add Product
 <section class="section">
     <div class="section-header">
       <div class="section-header-back">
-        <a href="{{ route('admin.index.product') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+        <a href="features-posts.html" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
       </div>
-      <h1>Create New Product</h1>
+      <h1>Create New Post</h1>
       <div class="section-header-breadcrumb">
-        <div class="breadcrumb-item active"><a href="#">Product</a></div>
-        <div class="breadcrumb-item">Create New Product</div>
+        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+        <div class="breadcrumb-item"><a href="#">Posts</a></div>
+        <div class="breadcrumb-item">Create New Post</div>
       </div>
     </div>
 
     <div class="section-body">
-      <h2 class="section-title">Create New Product</h2>
+      <h2 class="section-title">Create New Post</h2>
       <p class="section-lead">
-        On this page you can create a new Product and fill in all fields.
+        On this page you can create a new post and fill in all fields.
       </p>
 
       <div class="row">
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h4>Write Your Product</h4>
+              <h4>Write Your Post</h4>
             </div>
-            <form action="{{ Route('admin.store.product') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="card-body">
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Product Name</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" name="Product_name" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
-                      <select class="col-sm-12 col-md-7">
-                        <option>Option 1</option>
-                        <option>Option 2</option>
-                        <option>Option 3</option>
-                      </select>
-                    </div>
-                    <div class="form-group row mb-8">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
-                      <div class="col-sm-12 col-md-7">
-                        <textarea class="summernote-simple"></textarea>
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Foto</label>
-                      <div class="col-sm-12 col-md-7">
-                        <div id="image-preview" class="image-preview">
-                          <label for="image-upload" id="image-label">Choose File</label>
-                          <input type="file" name="image_product" id="image-upload">
-                        </div>
-                      </div>
-                    </div>
-                  <div class="form-group row mb-4">
-                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Price</label>
-                    <div class="col-sm-12 col-md-7">
-                      <input type="text" name="price" class="form-control">
-                    </div>
+            <div class="card-body">
+              <div class="form-group row mb-4">
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
+                <div class="col-sm-12 col-md-7">
+                  <input type="text" class="form-control">
                 </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-                      <div class="col-sm-12 col-md-7">
-                        <button class="btn btn-primary" type="submit">Create Product</button>
-                      </div>
-                    </div>
+              </div>
+              <div class="form-group row mb-4">
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
+                <div class="col-sm-12 col-md-7">
+                  <select class="form-control selectric">
+                    <option>Tech</option>
+                    <option>News</option>
+                    <option>Political</option>
+                  </select>
+                </div>
+              </div>
+
+              <div class="form-group row mb-4">
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
+                <div class="col-sm-12 col-md-7">
+                  <textarea class="summernote-simple" style="display: none;"></textarea><div class="note-editor note-frame card"><div class="note-dropzone">  <div class="note-dropzone-message"></div></div><div class="note-toolbar-wrapper" style="height: 36px;"><div class="note-toolbar card-header" style="position: relative; top: 0px; width: 100%;"><div class="note-btn-group btn-group note-style"><button type="button" class="note-btn btn btn-light btn-sm note-btn-bold" tabindex="-1" title="" data-original-title="Bold (CTRL+B)"><i class="note-icon-bold"></i></button><button type="button" class="note-btn btn btn-light btn-sm note-btn-italic" tabindex="-1" title="" data-original-title="Italic (CTRL+I)"><i class="note-icon-italic"></i></button><button type="button" class="note-btn btn btn-light btn-sm note-btn-underline" tabindex="-1" title="" data-original-title="Underline (CTRL+U)"><i class="note-icon-underline"></i></button><button type="button" class="note-btn btn btn-light btn-sm" tabindex="-1" title="" data-original-title="Remove Font Style (CTRL+\)"><i class="note-icon-eraser"></i></button></div><div class="note-btn-group btn-group note-font"><button type="button" class="note-btn btn btn-light btn-sm note-btn-strikethrough" tabindex="-1" title="" data-original-title="Strikethrough (CTRL+SHIFT+S)"><i class="note-icon-strikethrough"></i></button></div><div class="note-btn-group btn-group note-para"><div class="note-btn-group btn-group"><button type="button" class="note-btn btn btn-light btn-sm dropdown-toggle" tabindex="-1" data-toggle="dropdown" title="" data-original-title="Paragraph"><i class="note-icon-align-left"></i></button><div class="dropdown-menu"><div class="note-btn-group btn-group note-align"><button type="button" class="note-btn btn btn-light btn-sm" tabindex="-1" title="" data-original-title="Align left (CTRL+SHIFT+L)"><i class="note-icon-align-left"></i></button><button type="button" class="note-btn btn btn-light btn-sm" tabindex="-1" title="" data-original-title="Align center (CTRL+SHIFT+E)"><i class="note-icon-align-center"></i></button><button type="button" class="note-btn btn btn-light btn-sm" tabindex="-1" title="" data-original-title="Align right (CTRL+SHIFT+R)"><i class="note-icon-align-right"></i></button><button type="button" class="note-btn btn btn-light btn-sm" tabindex="-1" title="" data-original-title="Justify full (CTRL+SHIFT+J)"><i class="note-icon-align-justify"></i></button></div><div class="note-btn-group btn-group note-list"><button type="button" class="note-btn btn btn-light btn-sm" tabindex="-1" title="" data-original-title="Outdent (CTRL+[)"><i class="note-icon-align-outdent"></i></button><button type="button" class="note-btn btn btn-light btn-sm" tabindex="-1" title="" data-original-title="Indent (CTRL+])"><i class="note-icon-align-indent"></i></button></div></div></div></div></div></div><div class="note-editing-area"><div class="note-handle"><div class="note-control-selection" style="display: none;"><div class="note-control-selection-bg"></div><div class="note-control-holder note-control-nw"></div><div class="note-control-holder note-control-ne"></div><div class="note-control-holder note-control-sw"></div><div class="note-control-sizing note-control-se"></div><div class="note-control-selection-info"></div></div></div><textarea class="note-codable"></textarea><div class="note-editable card-block" contenteditable="true" style="min-height: 150px;"><p><br></p></div></div><div class="note-statusbar">  <div class="note-resizebar">    <div class="note-icon-bar"></div>    <div class="note-icon-bar"></div>    <div class="note-icon-bar"></div>  </div></div></div>
+                </div>
+              </div>
+
+              <div class="form-group row mb-4">
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
+                <div class="col-sm-12 col-md-7">
+                  <div id="image-preview" class="image-preview">
+                    <label for="image-upload" id="image-label">Choose File</label>
+                    <input type="file" name="image" id="image-upload" />
                   </div>
-
-            </form>
-
-
+                </div>
+              </div>
+              <div class="form-group row mb-4">
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tags</label>
+                <div class="col-sm-12 col-md-7">
+                  <input type="text" class="form-control inputtags">
+                </div>
+              </div>
+              <div class="form-group row mb-4">
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
+                <div class="col-sm-12 col-md-7">
+                  <select class="form-control selectric">
+                    <option>Publish</option>
+                    <option>Draft</option>
+                    <option>Pending</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group row mb-4">
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                <div class="col-sm-12 col-md-7">
+                  <button class="btn btn-primary">Create Post</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
