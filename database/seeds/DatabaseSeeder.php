425602756c251bc9a3e7ad2fb230d050ab9b1d02
muamar <muamarhumaidi@gmail.com>
2020-06-26 18:46:57 +0700

    add databse seeder

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+        $this->call(DistrictTableSeeder::class);
+        $this->call(ProvinceTableSeeder::class);
+        $this->call(CityTableSeeder::class);
+    }
+}
